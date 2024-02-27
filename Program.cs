fefb9a99fc5e9342bac34b407df4be300d51f1cb
vilarj <lamaravillasan@outlook.com>
2024-02-26 21:50:12 -0500

    Documenting swagger ui

--- Program.cs
+++ Program.cs
@@ -1,8 +1,31 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "loans-api",
+        Version = "v1",
+        Description = "An API to perform person operations",
+        Contact = new OpenApiContact
+        {
+            Name = "vilarj",
+            Email = "vilarj@x.com",
+            Url = new Uri("https://x.com/vilarjimenez"),
+        },
+        License = new OpenApiLicense
+        {
+            Name = "loans-api",
+            Url = new Uri("https://example.com/license"),
+        },
+
+    });
+});
 
 var app = builder.Build();
 
